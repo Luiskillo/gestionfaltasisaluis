86d1895de558618f269ea6299ec7bc4aee6ecceb
Luiskillo <luissilvacastro19@gmail.com>
2022-01-24 10:56:22 +0100

    initial commit

---
+++ BDConnector.php
@@ -0,0 +1,47 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instituto faltas - BDConnector</title>
+</head>
+<body>
+
+<?php
+
+require 'Usuario.php';
+
+class ConectorBD {
+
+    private $servername;
+    private $database;
+    private $user;
+    private $pass;
+
+    function __construct() {
+        $this->servername = "localhost";
+        $this->database = "gestionfaltasisaluis";
+        $this->user = "user";
+        $this->pass = "pass";
+    }
+
+    public function conectarUsuario($nombre) {
+        $conector = mysqli_connect($this->servername, $this->user, $this->pass, $this->database);
+        $sql = "select * from usuario where pk_name_prof = '$nombre'";
+        $resultado = mysqli_query($conector, $sql);
+
+        if ($nombre=='') {
+            echo "ERROR » DEBE INTRODUCIRSE UN NOMBRE DE USUARIO<br>";
+        } else if ($resultado->num_rows == 1) {
+
+            echo "USUARIO CONECTADO<br>";
+            //entrar a la bbdd de las faltas
+
+        } else {
+            echo "EL USUARIO « $nombre » NO EXISTE<br>";
+        }
+        mysqli_close($conector);
+    }
+}
+
+?>
+</body>
---
+++ servidor.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Instituto faltas - Servidor</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+
+<body>
+
+<?php
+require 'BDConnector.php';
+
+    $option = $_POST['option'];
+    //Hay que ver como sacar el rol de profesor/director
+
+    $conector = new ConectorBD();
+
+    switch ($option) {
+        case "inicio_sesion":
+            $nombre = $_POST['nombre'];
+
+            $conector->conectarUsuario($nombre);
+            break;
+    }
+?>
+
+<br><button><a href="login.html">Volver al inicio</a></button>
+</body>
